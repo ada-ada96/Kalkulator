3861884c0357dd83f0733286b331bdf3c2831416
Adrianna <ada@gmail.com>
2019-04-08 12:10:13 +0200

    porzadek

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,11 @@
 print("Witaj w programie kalkulator")
 print("Podaj dwie liczby do kalkulatora")
 
-
 def dodawanie(a, b):
 	wynik = a + b
 return wynik
+
+a = int(input())
+b = int(input())
+
+dodawanie(a, b)
